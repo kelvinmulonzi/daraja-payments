4ded151b0900a140c77dbf63f049a5f575c52a11
kelvinmulonzi <2003724@students.kcau.ac.ke>
2025-03-17 21:49:59 +0300

    initial

---
+++ src/main/java/com/example/daraja/payment/DarajaPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.daraja.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarajaPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarajaPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daraja/payment/DarajaPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daraja.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarajaPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
